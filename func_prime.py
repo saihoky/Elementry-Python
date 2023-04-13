290532d0f100cc94a6eb80c3434a167dbd28bcce
Hokey <48615551+saihoky@users.noreply.github.com>
2023-04-13 16:38:21 +0900

    Update func_prime.py

--- func_prime.py
+++ func_prime.py
@@ -1,16 +1,15 @@
-# file name: func_prime.py
+# file name: func_prime2.py
 
-def prime(num):
-    num_set = list(range(2, num + 1))
-    prime_set = num_set[:]
-    
-    for a in num_set:
-        for b in num_set:
-            if (b in prime_set) and (a != b and b % a == 0):
-                prime_set.remove(b)
-         
-    print(prime_set)
-    return prime_set
+def prime(range_num):
+    num_list = list(range(2, range_num + 1))
+
+    index = 0
+    while index < len(num_list):
+        num = num_list[index]
+        num_list = list(filter(lambda x: x == num or x % num, num_list))
+        index += 1
+
+    print(num_list)
 
 if __name__ == '__main__':
-    a=prime(int(input('Input the last number of a range what you want! ')))
+    a=prime(int(input('Input the last number of a range what you want!  ')))
